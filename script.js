a3d534048f0d0ca0514f49c33c99ec665b8071db
sagar kumar <94098920+Sagar-Kumar01@users.noreply.github.com>
2025-12-25 11:27:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+const square = document.getElementsByClassName('square');
+
+for(let i = 0; i<3; i++){
+	square[i].addEventListener("mouseover",()=>{
+		if(square[i].id == "square1"){
+			erase();
+			square[1].style.backgroundColor = "#6F4E37"
+			square[2].style.backgroundColor = "#6F4E37"
+		}else if(square[i].id == "square2"){
+			erase();
+			square[0].style.backgroundColor = "#6F4E37"
+			square[2].style.backgroundColor = "#6F4E37"
+		}else{
+			erase();
+			square[0].style.backgroundColor = "#6F4E37"
+			square[1].style.backgroundColor = "#6F4E37"
+		}
+	})
+}
+function erase() {
+	for(let i = 0; i<3; i++){
+		square[i].style.backgroundColor = "#E6E6FA"
+	}
+}
